10cb23712f2d47a6dc3412bab1ae58df3e71953a
SamiErenBayhan <155374516+SamiErenBayhan@users.noreply.github.com>
2026-06-26 01:32:54 +0300

    3

--- pipe.gd
+++ pipe.gd
@@ -7,3 +7,6 @@ func _physics_process(delta):
 	position.x -= speed * delta
 	if position.x < -950:
 		queue_free() 
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
